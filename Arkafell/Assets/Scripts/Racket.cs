b31b065000ec50dbe9aefd63845273b754f93a96
Unknown <isa.montero92@gmail.com>
2019-05-17 19:42:16 +0200

    Control raqueta
    Tras el seguimiento de varios tutoriales de internet y de mil formas diferentes de desarrollar el código la raqueta no se me mueve.

---
+++ Arkafell/Assets/Scripts/Racket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    // Le damos valor a la velocidad
+
+    public float speed = 10;
+    public float forced = 2000;
+    public Rigidbody2D rk;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rk = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("horizontal");
+        Vector2 vector = new Vector2(h, 0);
+
+        //Añadimos la fuerza al racket
+
+        rk.AddForce(vector * forced * Time.deltaTime);
+
+        // limitamos la velocidad
+        rk.velocity = new Vector2(Mathf.Clamp(rk.velocity.x, -speed, speed), 0);
+    }
+}
