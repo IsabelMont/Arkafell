0ddded6bad5a5772e328acab842c4bd4d07bc197
Unknown <isa.montero92@gmail.com>
2019-05-18 02:13:41 +0200

    Raqueta + Pelota + Suelo y reposicionamiento
    La raqueta funciona ( Hay que modificar la velocidad y la fuerza) La pelota se reposiciona y muere gracias al nuevo código suelo.

--- Arkafell/Assets/Scripts/Racket.cs
+++ Arkafell/Assets/Scripts/Racket.cs
@@ -8,18 +8,38 @@ public class Racket : MonoBehaviour
 
     public float speed = 10;
     public float forced = 2000;
-    public Rigidbody2D rk;
+    private Rigidbody2D rk;
+    public float velPelota = 10;
+    //Referencia a pelota
+    public GameObject pelota;
+    private Rigidbody2D rkPelota;
 
-    // Start is called before the first frame update
+    public bool pelotaMuerta = true;
+
     void Awake()
     {
         rk = GetComponent<Rigidbody2D>();
     }
 
+    //Usamos el start para recuperar el Rigibody de la pelota
+    private void Start()
+    {
+        rkPelota = pelota.GetComponent<Rigidbody2D>();
+    }
+
+    // Para detectar la tecla espacio
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)&& pelotaMuerta)
+        {
+            LanzarPelota();
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        float h = Input.GetAxis("horizontal");
+        float h = Input.GetAxis("Horizontal");
         Vector2 vector = new Vector2(h, 0);
 
         //Añadimos la fuerza al racket
@@ -28,5 +48,19 @@ public class Racket : MonoBehaviour
 
         // limitamos la velocidad
         rk.velocity = new Vector2(Mathf.Clamp(rk.velocity.x, -speed, speed), 0);
+
+        // Marcamos los limites en los que se va a poder mover el racket, para que no salga de pantalla.
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -8.5f, 8.5f), transform.position.y);
+    }
+
+    //Función para lanzar la pelota
+    void LanzarPelota()
+    {
+        //Decimos que la pelota se ha lanzado
+        pelotaMuerta = false;
+        //Decimos que el Rigibody de la pelota no es kinematico
+        rkPelota.isKinematic = false;
+        rkPelota.AddForce(new Vector2(0, velPelota));
     }
 }
---
+++ Arkafell/Assets/Suelo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suelo : MonoBehaviour
+{
+    public Vector2 posInicial;
+    public GameObject pelota;
+    public GameObject racket;
+    private Rigidbody2D rkPelota;
+    public Racket RacketScript;
+
+
+
+    void Start()
+    {
+        posInicial = pelota.transform.position;
+        rkPelota = pelota.GetComponent<Rigidbody2D>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "pelota")
+        {
+            //Cuando la pelota toque el suelo, la devolvemos a su posición inicial.
+            pelota.transform.position = new Vector3(posInicial.x+racket.transform.position.x, pelota.transform.position.y+2.0f, pelota.transform.position.z); 
+
+            //Le quitamos la velocidad a la pelota para que empiece desde cero.
+            pelota.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+            rkPelota.isKinematic = true;
+
+            //Cambiamos el valor de pelotaMuerta a true para saber que esta muerta.
+            RacketScript.pelotaMuerta = true;
+        }
+    }
+
+
+}
